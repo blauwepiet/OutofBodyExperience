638c98d312676343ec584902c8b528d1f90dcc3a
Paul Melis <paul.melis@surfsara.nl>
2016-10-27 18:54:48 +0200

    Only mem leak

--- Source/LeapTest/UDPImageStreamer.cpp
+++ Source/LeapTest/UDPImageStreamer.cpp
@@ -38,7 +38,7 @@ UTexture2D* UUDPImageStreamer::createDynamicOutputTex(UTexture2D *tex) {
 	FColor* pixels = (FColor*)rawImageData->Lock(LOCK_READ_WRITE);
 	int32 numPixels = dynamicTex->GetSizeX() * dynamicTex->GetSizeY();
 
-	//FMemory::Memzero(dymTexData, numPixels * 4);
+	dymTexData = (uint8*)FMemory::Malloc(numPixels * 4 * sizeof(uint8));
 
 	for (int p = 0; p < numPixels; p++) {
 		pixels[p] = FColor::MakeRedToGreenColorFromScalar(1.0 / numPixels);
@@ -95,11 +95,12 @@ bool UUDPImageStreamer::initSending(FString IP, int32 port)
 	RemoteAddr->SetIp(*IP, bIsValid);
 	RemoteAddr->SetPort(port);
 
-	if (!bIsValid)
+	UDPSender = new FUdpSocketSenderBinary(socket, TEXT("UDP SENDER"), RemoteAddr);
+	/*if (!bIsValid)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "IP address was not valid!");
 		return false;
-	}
+	}*/
 
 	return true;
 }
@@ -147,7 +148,8 @@ bool UUDPImageStreamer::sendSegment(FImageSegmentPackage data)
 	FArrayWriter Writer;
 	Writer << data;
 	int32 BytesSent = 0;
-	socket->Wait(ESocketWaitConditions::Type::WaitForReadOrWrite, 1000);
+	//std::this_thread::sleep_for(std::chrono::microseconds(50));
+	//UDPSender->Send(*data, Endpoint);
 	socket->SendTo(Writer.GetData(), Writer.Num(), BytesSent, *RemoteAddr);
 
 	if (BytesSent <= 0)
@@ -162,27 +164,86 @@ bool UUDPImageStreamer::sendSegment(FImageSegmentPackage data)
 	return true;
 }
 
-bool UUDPImageStreamer::sendBinaryData(uint8* data, uint32 packageLength) {
+bool UUDPImageStreamer::sendBinaryData(const TSharedRef<TArray<uint8, TFixedAllocator<PACKAGESIZE>>, ESPMode::ThreadSafe>& data, uint32 packageLength) {
 	if (!socket) {
 		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Socket not initialized");
 		return false;
 	}
 
 	int32 BytesSent = 0;
-	std::this_thread::sleep_for(std::chrono::microseconds(50));
-	socket->SendTo(data, packageLength, BytesSent, *RemoteAddr);
+	//std::this_thread::sleep_for(std::chrono::microseconds(1));
+	//socket->SendTo(data, packageLength, BytesSent, *RemoteAddr);
+	UDPSender->Send(data);
 
-	if (BytesSent <= 0)
-	{
-		//UE_LOG(UDPImageStreamerLogger, Log, TEXT("Invalid send because: "));
-		//UE_LOG(UDPImageStreamerLogger, Log, TEXT("Tried to send writer with size %i and data: %s"), Writer.Num(), *FString::FromBlob(Writer.GetData(), PACKAGESIZE));
+	//if (BytesSent <= 0)
+	//{
+	//	//UE_LOG(UDPImageStreamerLogger, Log, TEXT("Invalid send because: "));
+	//	//UE_LOG(UDPImageStreamerLogger, Log, TEXT("Tried to send writer with size %i and data: %s"), Writer.Num(), *FString::FromBlob(Writer.GetData(), PACKAGESIZE));
 
-		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "invalid send");
-		return false;
-	}
+	//	GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "invalid send");
+	//	return false;
+	//}
 	return true;
 }
 
+//bool UUDPImageStreamer::sendImage(UTexture2D *tex)
+//{
+//	if (!tex) {
+//		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Tex is null ptr");
+//		return false;
+//	}
+//
+//	textureToSend = tex;
+//
+//	FByteBulkData* rawImageData = &tex->PlatformData->Mips[0].BulkData;
+//
+//	if (!&rawImageData) {
+//		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Rawimagedata is null ptr");
+//		return false;
+//	}
+//
+//	uint8* formatedImageData = (uint8*)rawImageData->Lock(LOCK_READ_WRITE);
+//
+//	if (!formatedImageData) {
+//		GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Formatedimagedata is null ptr");
+//		rawImageData->Unlock();
+//		return false;
+//	}
+//
+//	uint32 textureWidth = tex->GetSizeX(), textureHeight = tex->GetSizeX();
+//	nrOfBytesToSend = textureWidth * textureHeight * 4;
+//	nrOfPackagesToSend = (nrOfBytesToSend + BUFFERSIZE - 1) / BUFFERSIZE;
+//
+//	FImageSegmentPackage imageSegmentPackage;
+//
+//	imageSegmentPackage.frameTime = ((float)(clock() - classInitTime))/CLOCKS_PER_SEC;
+//	//float frameTime = ((float)(clock() - classInitTime)) / CLOCKS_PER_SEC;
+//	clock_t start = clock();
+//	for (uint32 i = 0; i < nrOfPackagesToSend; i++) {
+//
+//		clock_t memClock = clock();
+//		imageSegmentPackage.idx = i;
+//		uint8* segmentDataPtr = imageSegmentPackage.imageData;
+//		//uint8* segmentDataPtr = formatedImageData + i*BUFFERSIZE;
+//		uint8* imageDataSegment = formatedImageData + i*BUFFERSIZE;
+//		int32 residue = (i*BUFFERSIZE + BUFFERSIZE) - nrOfBytesToSend;
+//		if (residue < 0) {
+//			FMemory::Memcpy(segmentDataPtr, imageDataSegment, BUFFERSIZE);
+//		}
+//		else {
+//			FMemory::Memcpy(segmentDataPtr, imageDataSegment, BUFFERSIZE-residue);
+//		}
+//		if (i == 1)UE_LOG(UDPImageStreamerLogger, Log, TEXT("memclock took: %s"), *FString::SanitizeFloat(((double)(clock() - memClock)) / CLOCKS_PER_SEC));
+//		sendSegment(imageSegmentPackage);
+//		
+//	}
+//	UE_LOG(UDPImageStreamerLogger, Log, TEXT("It took %f seconds"), ((float)(clock() - start)) / CLOCKS_PER_SEC);
+//	rawImageData->Unlock();
+//
+//	return true;
+//}
+
+//NEWCODE BUT COMMENTED FOR TESTING
 bool UUDPImageStreamer::sendImage(UTexture2D *tex)
 {
 	if (!tex) {
@@ -216,43 +277,35 @@ bool UUDPImageStreamer::sendImage(UTexture2D *tex)
 	//imageSegmentPackage.frameTime = ((float)(clock() - classInitTime))/CLOCKS_PER_SEC;
 	float frameTime = ((float)(clock() - classInitTime)) / CLOCKS_PER_SEC;
 	clock_t start = clock();
-	uint8 package[PACKAGESIZE];
-	uint8* packagePtr = package;
-	float* fltPackagePtr = (float*)packagePtr;
-	uint32* intPackagePtr = (uint32*)packagePtr;
-	fltPackagePtr[0] = frameTime;
-	UE_LOG(UDPImageStreamerLogger, Log, TEXT("Frame time: %s %f"), *FString::FromBlob(packagePtr, 4), frameTime);
+	//uint8 package[PACKAGESIZE];
+	
+	//UE_LOG(UDPImageStreamerLogger, Log, TEXT("Frame time: %s %f"), *FString::FromBlob(packagePtr, 4), frameTime);
 	for (uint32 i = 0; i < nrOfPackagesToSend; i++) {
-		intPackagePtr[1] = i; 
+		TSharedRef<TArray<uint8, TFixedAllocator<PACKAGESIZE>>, ESPMode::ThreadSafe> package(new TArray<uint8, TFixedAllocator<PACKAGESIZE>>());
+		uint8* packagePtr = package.Get().GetData();
+		float* fltPackagePtr = (float*)packagePtr;
+		uint32* intPackagePtr = (uint32*)packagePtr;
+		fltPackagePtr[0] = frameTime;
+		intPackagePtr[1] = i;
 		//packagePtr = &package[4];
 		//packagePtr = reinterpret_cast<uint8_t*>(&i);
+		clock_t memClock = clock();
 		int32 residue = (i*BUFFERSIZE + BUFFERSIZE) - nrOfBytesToSend;
 		if (residue < 0) {
-			FMemory::Memcpy(&package[8], formatedImageData + i*BUFFERSIZE, BUFFERSIZE);
+			FMemory::Memcpy(&packagePtr[8], formatedImageData + i*BUFFERSIZE, BUFFERSIZE);
 		}
 		else {
-			FMemory::Memcpy(&package[8], formatedImageData + i*BUFFERSIZE, BUFFERSIZE-residue);
+			FMemory::Memcpy(&packagePtr[8], formatedImageData + i*BUFFERSIZE, BUFFERSIZE - residue);
 		}
 
-		if(i == 1) UE_LOG(UDPImageStreamerLogger, Log, TEXT("Sending: %s"), *FString::FromBlob(package, PACKAGESIZE));
-
+		//if (i == 1) UE_LOG(UDPImageStreamerLogger, Log, TEXT("Sending: %s"), *FString::FromBlob(package.Get().GetData(), PACKAGESIZE));
+		//if (i == 1)UE_LOG(UDPImageStreamerLogger, Log, TEXT("memclock took: %s"), *FString::SanitizeFloat(((double)(clock() - memClock)) / CLOCKS_PER_SEC));
+		clock_t sendClock = clock();
 		sendBinaryData(package, PACKAGESIZE);
+		//if (i == 1)UE_LOG(UDPImageStreamerLogger, Log, TEXT("sending took: %s"), *FString::SanitizeFloat(((double)(clock() - sendClock)) / CLOCKS_PER_SEC));
 
-		//OLDCODE
-		//imageSegmentPackage.idx = i;
-		//uint8* segmentDataPtr = imageSegmentPackage.imageData;
-		////uint8* segmentDataPtr = formatedImageData + i*BUFFERSIZE;
-		//uint8* imageDataSegment = formatedImageData + i*BUFFERSIZE;
-		//int32 residue = (i*BUFFERSIZE + BUFFERSIZE) - nrOfBytesToSend;
-		//if (residue < 0) {
-		//	FMemory::Memcpy(segmentDataPtr, imageDataSegment, BUFFERSIZE);
-		//}
-		//else {
-		//	FMemory::Memcpy(segmentDataPtr, imageDataSegment, BUFFERSIZE-residue);
-		//}
-		//sendSegment(imageSegmentPackage);
 	}
-	UE_LOG(UDPImageStreamerLogger, Log, TEXT("It took %f seconds"), ((float)(clock() - start)) / CLOCKS_PER_SEC);
+	//UE_LOG(UDPImageStreamerLogger, Log, TEXT("It took %f seconds"), ((float)(clock() - start)) / CLOCKS_PER_SEC);
 	rawImageData->Unlock();
 
 	return true;
@@ -264,6 +317,12 @@ void UUDPImageStreamer::closeConnection()
 	{
 		delete UDPReceiver;
 		UDPReceiver = nullptr;
+	}
+
+	if (UDPSender)
+	{
+		delete UDPSender;
+		UDPSender = nullptr;
 	}
 
 	if (socket)
@@ -327,8 +386,8 @@ void UUDPImageStreamer::Recv(const FArrayReaderPtr& ArrayReaderPtr, const FIPv4E
 
 	nrOfPackagesReceived++;
 
-	FByteBulkData* rawImageData = &dynamicTex->PlatformData->Mips[0].BulkData;
-	uint8* imageData = (uint8*)rawImageData->Lock(LOCK_READ_WRITE);
+	//FByteBulkData* rawImageData = &dynamicTex->PlatformData->Mips[0].BulkData;
+	uint8* imageData = dymTexData; //(uint8*)rawImageData->Lock(LOCK_READ_WRITE);
 	uint8* imageDataFragment = imageData + idx*BUFFERSIZE;
 
 	int32 residue = (idx*BUFFERSIZE + BUFFERSIZE) - nrOfBytesToSend;
@@ -340,7 +399,7 @@ void UUDPImageStreamer::Recv(const FArrayReaderPtr& ArrayReaderPtr, const FIPv4E
 		//UE_LOG(UDPImageStreamerLogger, Log, TEXT("Received residue segment %d of size %d"), copy.idx, (BUFFERSIZE - residue - 1));
 		FMemory::Memcpy(imageDataFragment, &receivedPackage[8], BUFFERSIZE - residue);
 	}
-	rawImageData->Unlock();
+	//rawImageData->Unlock();
 
 	if (frameTime != prevFrameTime) {
 		this->OnSuccess.Broadcast();
@@ -350,25 +409,23 @@ void UUDPImageStreamer::Recv(const FArrayReaderPtr& ArrayReaderPtr, const FIPv4E
 }
 
 void UUDPImageStreamer::updateTexture() {
-	FByteBulkData* rawImageData = &dynamicTex->PlatformData->Mips[0].BulkData;
-	uint8* imageData = (uint8*)rawImageData->Lock(LOCK_READ_WRITE);
+	//FByteBulkData* rawImageData = &dynamicTex->PlatformData->Mips[0].BulkData;
+	//uint8* imageData = (uint8*)rawImageData->Lock(LOCK_READ_WRITE);
+	GEngine->AddOnScreenDebugMessage(-1, 0.033, FColor::Red, "updating tex");
+	//FByteBulkData* rawImageDataSendTex = &textureToSend->PlatformData->Mips[0].BulkData;
+	//if (!rawImageDataSendTex->IsLocked()) {
+		//uint8* imageDataSendTex = (uint8*)rawImageDataSendTex->Lock(LOCK_READ_WRITE);
 
-	FByteBulkData* rawImageDataSendTex = &textureToSend->PlatformData->Mips[0].BulkData;
-	if (!rawImageDataSendTex->IsLocked()) {
-		uint8* imageDataSendTex = (uint8*)rawImageDataSendTex->Lock(LOCK_READ_WRITE);
+		//GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "tosend snippet: " + FString::FromBlob(imageDataSendTex, 20));
+		//GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "dymtex snippet: " + FString::FromBlob(dymTexData, 20));
 
-		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "tosend snippet: " + FString::FromBlob(imageDataSendTex, 20));
-		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "dymtex snippet: " + FString::FromBlob(imageData, 20));
-
-		GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, "updating tex");
-
-		rawImageDataSendTex->Unlock();
-	}
+		//rawImageDataSendTex->Unlock();
+	//}
 
 	ENQUEUE_UNIQUE_RENDER_COMMAND_FOURPARAMETER(
 		UpdateDynamicTextureCode,
 		UTexture2D*, pTexture, dynamicTex,
-		const uint8*, pData, imageData,
+		const uint8*, pData, dymTexData,
 		uint32, width, dynamicTex->GetSizeX(),
 		uint32, height, dynamicTex->GetSizeY(),
 		{
@@ -384,5 +441,5 @@ void UUDPImageStreamer::updateTexture() {
 			RHIUpdateTexture2D(resource->GetTexture2DRHI(), 0, region, region.Width * 4, pData);
 		});
 
-	rawImageData->Unlock();
+	//rawImageData->Unlock();
 }
--- Source/LeapTest/UDPImageStreamer.h
+++ Source/LeapTest/UDPImageStreamer.h
@@ -6,6 +6,8 @@
 #include "Networking.h"
 #include "Engine.h"
 #include <time.h>
+#include <chrono>
+#include <thread>
 
 #include "Components/ActorComponent.h"
 #include "UDPImageStreamer.generated.h"
@@ -41,6 +43,191 @@ FORCEINLINE FArchive& operator<<(FArchive &Ar, FImageSegmentPackage& imageSegmen
 	return Ar;
 }
 
+/**
+* Asynchronously sends data to an UDP socket.
+*
+* @todo networking: gmp: implement rate limits
+*/
+class FUdpSocketSenderBinary
+	: public FRunnable
+{
+
+public:
+
+	/**
+	* Creates and initializes a new socket sender.
+	*
+	* @param InSocket The UDP socket to use for sending data.
+	* @param ThreadDescription The thread description text (for debugging).
+	*/
+	FUdpSocketSenderBinary(FSocket* InSocket, const TCHAR* ThreadDescription, TSharedPtr<FInternetAddr>	RemoteAddr)
+		: SendRate(0)
+		, Socket(InSocket)
+		, Stopping(false)
+		, WaitTime(FTimespan::FromMilliseconds(100))
+		, RemoteAddr(RemoteAddr)
+	{
+		check(Socket != nullptr);
+		check(Socket->GetSocketType() == SOCKTYPE_Datagram);
+
+		WorkEvent = FPlatformProcess::GetSynchEventFromPool();
+		Thread = FRunnableThread::Create(this, ThreadDescription, 128 * 1024, TPri_AboveNormal, FPlatformAffinity::GetPoolThreadMask());
+	}
+
+	/** Virtual destructor. */
+	virtual ~FUdpSocketSenderBinary()
+	{
+		if (Thread != nullptr)
+		{
+			Thread->Kill(true);
+			delete Thread;
+		}
+
+		FPlatformProcess::ReturnSynchEventToPool(WorkEvent);
+		WorkEvent = nullptr;
+	}
+
+public:
+
+	/**
+	* Gets the maximum send rate (in bytes per second).
+	*
+	* @return Current send rate.
+	*/
+	uint32 GetSendRate() const
+	{
+		return SendRate;
+	}
+
+	/**
+	* Gets the current throughput (in bytes per second).
+	*
+	* @return Current throughput.
+	*/
+	uint32 GetThroughput() const
+	{
+		return Throughput;
+	}
+
+	/**
+	* Sends data to the specified recipient.
+	*
+	* @param Data The data to send.
+	* @param Recipient The recipient.
+	* @return true if the data will be sent, false otherwise.
+	*/
+	bool Send(const TSharedRef<TArray<uint8, TFixedAllocator<PACKAGESIZE>>, ESPMode::ThreadSafe>& Data)
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Queueing: " + FString::FromBlob(Data,20));
+		if (!Stopping && SendQueue.Enqueue(Data))
+		{
+			WorkEvent->Trigger();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	* Sets the send rate (in bytes per second).
+	*
+	* @param Rate The new send rate (0 = unlimited).
+	* @see SetWaitTime
+	*/
+	void SetSendRate(uint32 Rate)
+	{
+		SendRate = Rate;
+	}
+
+	/**
+	* Sets the maximum time span to wait for work items.
+	*
+	* @param Timespan The wait time.
+	* @see SetSendRate
+	*/
+	void SetWaitTime(const FTimespan& Timespan)
+	{
+		WaitTime = Timespan;
+	}
+
+public:
+
+	// FRunnable interface
+
+	virtual bool Init() override
+	{
+		return true;
+	}
+
+	virtual uint32 Run() override
+	{
+		while (!Stopping)
+		{
+			while (!SendQueue.IsEmpty())
+			{
+				if (Socket->Wait(ESocketWaitConditions::WaitForWrite, WaitTime))
+				{
+					TSharedPtr<TArray<uint8, TFixedAllocator<PACKAGESIZE>>, ESPMode::ThreadSafe> Packet;
+					int32 Sent = 0;
+
+					SendQueue.Dequeue(Packet);
+					//GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Queueing: " + FString::FromBlob(Packet, 20));
+					Socket->SendTo(Packet->GetData(), PACKAGESIZE, Sent, *RemoteAddr);
+					Packet->Empty();
+					Packet.Reset();
+					if (Sent <= 0)
+					{
+						GEngine->AddOnScreenDebugMessage(-1, 3, FColor::Red, "Invalid sent");
+						//return 0;
+					}
+				}
+			}
+
+			WorkEvent->Wait(WaitTime);
+		}
+
+		return 0;
+	}
+
+	virtual void Stop() override
+	{
+		Stopping = true;
+		WorkEvent->Trigger();
+	}
+
+	virtual void Exit() override { }
+
+private:
+
+	/** Holds the send queue. */
+	TQueue<TSharedPtr<TArray<uint8, TFixedAllocator<PACKAGESIZE>>, ESPMode::ThreadSafe>, EQueueMode::Mpsc> SendQueue;
+
+	/** Holds the send rate. */
+	uint32 SendRate;
+
+	/** Holds the network socket. */
+	FSocket* Socket;
+
+	/** Holds a flag indicating that the thread is stopping. */
+	bool Stopping;
+
+	/** Holds the thread object. */
+	FRunnableThread* Thread;
+
+	/** Holds the current throughput. */
+	uint32 Throughput;
+
+	/** Holds the amount of time to wait for inbound packets. */
+	FTimespan WaitTime;
+
+	/** Holds an event signaling that inbound messages need to be processed. */
+	FEvent* WorkEvent;
+
+	TSharedPtr<FInternetAddr>	RemoteAddr;
+};
+
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FGetReceivedData);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -51,6 +238,7 @@ class LEAPTEST_API UUDPImageStreamer : public UActorComponent
 private:
 	FSocket* socket;
 	FUdpSocketReceiver* UDPReceiver = nullptr;
+	FUdpSocketSenderBinary* UDPSender = nullptr;
 	clock_t classInitTime;
 
 	UTexture2D* dynamicTex;
@@ -66,6 +254,7 @@ private:
 		uint32 height;
 	} prevTexInfo;*/
 	float prevFrameTime;
+	clock_t startSending;
 
 public:	
 	// Sets default values for this component's properties
@@ -91,7 +280,7 @@ public:
 
 	bool sendSegment(FImageSegmentPackage data);
 
-	bool sendBinaryData(uint8* data, uint32 packageLength);
+	bool sendBinaryData(const TSharedRef<TArray<uint8, TFixedAllocator<PACKAGESIZE>>, ESPMode::ThreadSafe>& data, uint32 packageLength);
 
 	bool initSending(FString IP, int32 port);
 
